4dc9cf5f35227f2674ead964ac0d99257f6a46eb
Jim Nielsen <jimniels@gmail.com>
2019-09-08 13:55:16 -0600

    move images

---
+++ scripts/copy-from-ia-writer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp /Users/jimnielsen/Library/Mobile\ Documents/27N4MQEA55~pro~writer/Documents/blog/*.md src/client/posts/
+cp /Users/jimnielsen/Library/Mobile\ Documents/27N4MQEA55~pro~writer/Documents/blog/drafts/*.md src/client/drafts/
+cp -R /Users/jimnielsen/Library/Mobile\ Documents/27N4MQEA55~pro~writer/Documents/blog/images src/client/
