bda8a42efec410569f8a80dd68cfeb7897d3110f
Jim Nielsen <jimniels@gmail.com>
2019-11-01 22:28:49 -0600

    get rid of old code and notes

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# Jim Nielsen’s Blog
-
-[jim-nielsen.com/blog](http://jim-nielsen.com/blog)
-
-FYI Production branch is `gh-pages`.
+# [Jim Nielsen’s Blog](https://blog.jim-nielsen.com)
--- scripts/copy-to-ia-writer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp src/client/posts/*.md '/Users/jimnielsen/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/blog'
-cp src/client/drafts/*.md '/Users/jimnielsen/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/blog/drafts'
-cp -R src/client/images '/Users/jimnielsen/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/blog'
