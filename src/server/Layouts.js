0614386a8277a144922cf549a8905b093669b33f
Jim Nielsen <jimniels@gmail.com>
2020-02-16 23:05:20 -0700

    Add comment before doctype

--- src/server/Layouts.js
+++ src/server/Layouts.js
@@ -2,40 +2,57 @@ import pt from "prop-types";
 import Nav from "./Nav.js";
 import { html, toDateUI } from "./utils.js";
 
+const comment = `
+<!--
+
+
+
+👋
+Want to read the code behind this code?
+It’s available on GitHub.
+https://www.github.com/jimniels/blog/
+
+
+
+-->
+`;
+
 const Layout = (props, children) => {
   const {
     site: { origin, name, isDevelopment },
     page: { layout, permalink, title }
   } = props;
 
-  return html`
-    <!DOCTYPE html>
-    <html id="top">
-      <head>
-        <title>
-          ${title && `${title} | `}${name}
-        </title>
+  return (
+    comment +
+    html`
+      <!DOCTYPE html>
+      <html id="top">
+        <head>
+          <title>
+            ${title && `${title} | `}${name}
+          </title>
 
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href="https://cdn.jim-nielsen.com" />
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
-        <link
-          rel="alternate"
-          type="application/json"
-          title="JSON Feed"
-          href="/feed.json"
-        />
-        <link rel="stylesheet" href="/assets/css/normalize.css" />
-        <link rel="stylesheet" href="/assets/css/styles.css" />
-        ${layout === "Post" &&
-          `
+          <meta charset="utf-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="preconnect" href="https://cdn.jim-nielsen.com" />
+          <link
+            rel="alternate"
+            type="application/rss+xml"
+            title="RSS"
+            href="/feed.xml"
+          />
+          <link
+            rel="alternate"
+            type="application/json"
+            title="JSON Feed"
+            href="/feed.json"
+          />
+          <link rel="stylesheet" href="/assets/css/normalize.css" />
+          <link rel="stylesheet" href="/assets/css/styles.css" />
+          ${layout === "Post" &&
+            `
           <meta property="og:title" content="${title}" />
           <meta property="og:type" content="article" />
           <meta property="og:url" content="${origin + permalink}" />
@@ -47,20 +64,21 @@ const Layout = (props, children) => {
           <meta name="twitter:image" content="https://cdn.jim-nielsen.com/shared/twitter-card.jpg">
           <meta name="twitter:image:alt" content="Photo of Jim Nielsen saying stuff">
         `}
-      </head>
-      <body>
-        <nav class="nav">
-          ${Nav(props)}
-        </nav>
+        </head>
+        <body>
+          <nav class="nav">
+            ${Nav(props)}
+          </nav>
 
-        <main class="main">
-          ${children}
-        </main>
+          <main class="main">
+            ${children}
+          </main>
 
-        <script type="text/javascript" src="/assets/js/js.js"></script>
-      </body>
-    </html>
-  `;
+          <script type="text/javascript" src="/assets/js/js.js"></script>
+        </body>
+      </html>
+    `
+  );
 };
 
 const Post = props => {
