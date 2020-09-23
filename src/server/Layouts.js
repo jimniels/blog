a6406e97dbdbe2a65af4077c456ddb58558a8245
Jim Nielsen <jimniels@gmail.com>
2020-09-23 11:05:02 -0600

    remove twitter card images

--- src/server/Layouts.js
+++ src/server/Layouts.js
@@ -60,8 +60,12 @@ const Layout = (props, children) => {
             <meta name="twitter:site" content="@jimniels">
             <meta name="twitter:creator" content="@jimniels">
             <meta name="twitter:title" content="${title}">
-            <meta name="twitter:image" content="https://blog.jim-nielsen.com/assets/img/twitter-card.png">
-            <meta name="twitter:image:alt" content="Photo of Jim Nielsen saying stuff">
+            ${
+              "" /*
+              <meta name="twitter:image" content="https://blog.jim-nielsen.com/assets/img/twitter-card.png">
+              <meta name="twitter:image:alt" content="Photo of Jim Nielsen saying stuff">
+              */
+            }
           `}
         </head>
         <body>
