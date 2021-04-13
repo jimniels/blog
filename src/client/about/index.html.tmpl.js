09425d38c7bb944f10be3bbe5fa93d0148c435af
Jim Nielsen <jimniels@gmail.com>
2021-04-13 14:32:55 -0600

    feat: update links with favicons

--- src/client/about/index.html.tmpl.js
+++ src/client/about/index.html.tmpl.js
@@ -109,23 +109,27 @@ export default function About(site) {
           (domain) => html`
             <details>
               <summary>
-                ${domain}
-                <span style="float: right"
-                  >${site.linksByDomain[domain].length}</span
-                >
+                <img
+                  src="https://www.google.com/s2/favicons?domain=${domain}&sz=32"
+                  width="16"
+                  height="16"
+                  alt="Favicon for ${domain}"
+                />
+                <span class="domain">${domain}</span>
+                <span class="count">${site.linksByDomain[domain].length}</span>
               </summary>
-              <ul>
+              <ol>
                 ${site.linksByDomain[domain].map(
                   (link) => html` <li><a href="${link}">${link}</a></li> `
                 )}
-              </ul>
+              </ol>
             </details>
           `
         )}
 
         <details>
           <summary>...all others with only one occurence</summary>
-          <ul>
+          <ol>
             ${domainsOfOnlyOne.map(
               (d) => html`
                 <li>
@@ -135,15 +139,25 @@ export default function About(site) {
                 </li>
               `
             )}
-          </ul>
+          </ol>
         </details>
       </div>
       <style>
+        details {
+          border: 1px solid transparent;
+          border-radius: calc(var(--border-radius) + 1px);
+        }
+        details[open] {
+          border-color: var(--c-gray-6);
+        }
+        details[open] summary {
+          border-bottom-left-radius: 0;
+          border-bottom-right-radius: 0;
+        }
         details summary {
           font-size: 0.8181rem;
           background: var(--c-gray-6);
           padding: 2px 10px;
-          margin-bottom: 2px;
           border-radius: var(--border-radius);
           overflow-wrap: break-word;
           word-wrap: break-word;
@@ -152,8 +166,24 @@ export default function About(site) {
         details summary:hover {
           background: var(--c-gray-5);
         }
-        details ul {
+        details ol {
           font-size: 0.6363rem;
+        }
+        details ol a {
+          overflow-wrap: break-word;
+          word-wrap: break-word;
+          -ms-word-break: break-all;
+        }
+        summary img {
+          position: relative;
+          top: 2px;
+          margin-right: 6px;
+        }
+        summary .count {
+          float: right;
+          opacity: 0.5;
+        }
+        summary .domain {
         }
       </style>`
   );
