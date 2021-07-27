a8f7ee974d659630a5198507b13e8a643f748c1b
Jim Nielsen <jimniels@gmail.com>
2021-07-27 14:44:29 -0600

    try new home

--- src/client/index.html.tmpl.js
+++ src/client/index.html.tmpl.js
@@ -14,29 +14,140 @@ export default function Index(site) {
     .filter((post) => post.hasOwnProperty("pageviews"))
     .sort((a, b) => (a.pageviews > b.pageviews ? -1 : 1));
 
+  const filters = [
+    {
+      label: "All",
+      icon: "archive",
+      description: "Every post I’ve ever written, from most recent",
+    },
+    {
+      label: "Favorites",
+      icon: "star",
+      description: "My own personal favorites through time.",
+    },
+    {
+      label: "Popular",
+      icon: "trending",
+      description: "Most popular this month according to Netlify analytics.",
+    },
+  ];
+
   return PageCustom(
     { site, page },
     html`
       <h1>Posts</h1>
 
-      <h2>Latest</h2>
-      ${PostList(recent)} ${site.blogPostsStatus}
-
-      <h2>Select Personal Favorites</h2>
-      ${PostList(favorites)}
+      ${site.blogPostsStatus}
       ${trending.length > 0 &&
       html`
-        <h2>
-          Popular This Month
-          <small style="font-weight: normal">
-            (<a
-              href="/2020/using-netlify-analytics-to-build-list-of-popular-posts/"
-              >According to Netlify Analytics</a
-            >)</small
-          >
-        </h2>
-        ${PostList(trending)}
+        <!--<small style="font-weight: normal">
+          (<a
+            href="/2020/using-netlify-analytics-to-build-list-of-popular-posts/"
+            >According to Netlify Analytics</a
+          >)</small
+        >-->
       `}
+
+      <style>
+        [hidden] {
+          display: none !important;
+        }
+        form {
+          display: flex;
+          align-items: center;
+          margin: 1.618rem 0;
+          color: var(--c-primary);
+          gap: calc(1.618rem / 6);
+        }
+        form label {
+          display: flex;
+          align-items: center;
+          padding: calc(1.618rem / 6) calc(1.618rem / 3);
+          border-radius: var(--border-radius);
+        }
+        form label:hover {
+          background-color: var(--c-gray-6);
+        }
+        form input {
+          display: none;
+        }
+        form label svg {
+          margin-right: calc(1.618rem / 6);
+        }
+        form input:checked + label {
+          background-color: var(--c-primary);
+          color: white;
+        }
+      </style>
+      <form>
+        ${filters.map(
+          ({ label, icon }, i) => html`
+            <input
+              type="radio"
+              name="filters"
+              id="${i}"
+              value="${i}"
+              ${i === 0 ? "checked" : ""}
+            />
+            <label for="${i}"
+              ><svg class="icon">
+                <use xlink:href="#${icon}"></use></svg
+              >${label}</label
+            >
+          `
+        )}
+      </form>
+
+      ${PostList(site.posts)}
+
+      <script>
+        const $lis = Array.from(document.querySelectorAll(".posts-list li"));
+        document.querySelector("form").addEventListener("change", (e) => {
+          switch (e.target.value) {
+            case "0":
+              console.log("Show all");
+              $lis.forEach(($li) => {
+                $li.removeAttribute("hidden");
+              });
+              sortList("data-date");
+              break;
+            case "1":
+              console.log("Show favorites", $lis);
+              $lis.forEach(($li) => {
+                if ($li.hasAttribute("data-favorites")) {
+                  $li.removeAttribute("hidden");
+                } else {
+                  $li.setAttribute("hidden", true);
+                }
+              });
+              sortList("data-favorites");
+              break;
+            case "2":
+              console.log("Show popular");
+              $lis.forEach(($li) => {
+                if ($li.hasAttribute("data-pageviews")) {
+                  $li.removeAttribute("hidden");
+                } else {
+                  $li.setAttribute("hidden", true);
+                }
+              });
+              sortList("data-pageviews");
+              break;
+            default:
+          }
+        });
+
+        function sortList(attr) {
+          var ul = document.querySelector(".posts-list");
+
+          Array.from(ul.getElementsByTagName("LI"))
+            .sort(
+              (a, b) =>
+                Number(a.getAttribute(attr)) < Number(b.getAttribute(attr))
+            )
+            .forEach((li) => ul.appendChild(li));
+        }
+      </script>
     `
   );
 }
@@ -45,18 +156,20 @@ function PostList(posts) {
   return html`
     <ul class="posts-list">
       ${posts.map(
-        ({ permalink, title, pageviews, date }) => html`
-          <li>
-            <a href="${permalink}">${title}</a>
+        ({ favorites_index, permalink, title, pageviews, date }) => html`
+          <li
+            data-date="${date.toISOString().slice(0, 10).replace(/-/g, "")}"
+            ${favorites_index >= 0 && `data-favorites='${favorites_index}'`}
+            ${pageviews && `data-pageviews='${pageviews}'`}
+          >
             <time datetime="${date.toISOString()}">${toDateUI(date)}</time>
-            ${
-              /*pageviews &&
+            ${pageviews &&
             html`<small
               >${pageviews > 1000
                 ? Math.round((pageviews / 1000) * 10) / 10 + "k"
                 : pageviews}</small
-            >`*/ ""
-            }
+            >`}
+            <a href="${permalink}">${title}</a>
           </li>
         `
       )}
