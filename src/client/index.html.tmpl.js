4c35cd6958b2d1c85b2bf959cb0e10b67739d83e
Jim Nielsen <jimniels@gmail.com>
2022-01-05 14:29:24 -0700

    tweaks

--- src/client/index.html.tmpl.js
+++ src/client/index.html.tmpl.js
@@ -25,12 +25,12 @@ export default function Index(site) {
       description: "Every post I’ve ever written, from most recent",
     },
     {
-      label: "Favorites",
+      label: "★ Favorites",
       icon: "star",
       description: "My own personal favorites through time.",
     },
     {
-      label: "Popular",
+      label: "↗ Popular",
       icon: "trending",
       description: "Most popular this month according to Netlify analytics.",
     },
@@ -41,7 +41,14 @@ export default function Index(site) {
     html`
       <h1>Posts</h1>
 
-      ${site.blogPostsStatus}
+      <!-- <h2>Latest</h2>
+      ${PostList(recent)}
+
+      <h2>Personal Favorites</h2>
+      ${PostList(favorites)}
+
+      <h2>Popular</h2>
+      ${PostList(trending, true)} -->
       ${trending.length > 0 &&
       html`
         <!--<small style="font-weight: normal">
@@ -58,8 +65,13 @@ export default function Index(site) {
         }
         form {
           display: flex;
-          align-items: center;
+          flex-wrap: wrap;
+          gap: calc(1.618rem / 2);
           margin: 1.618rem 0;
+        }
+        form div {
+          display: flex;
+          align-items: center;
           color: var(--c-primary);
           gap: calc(1.618rem / 6);
         }
@@ -72,7 +84,7 @@ export default function Index(site) {
         form label:hover {
           background-color: var(--c-gray-6);
         }
-        form input {
+        form input[type="radio"] {
           display: none;
         }
         form label svg {
@@ -82,29 +94,52 @@ export default function Index(site) {
           background-color: var(--c-primary);
           color: white;
         }
+        form input[type="text"] {
+          background: var(--c-gray-6);
+          border-radius: var(--border-radius);
+          padding: calc(1.618rem / 6) calc(1.618rem / 3);
+          border: none;
+          width: 100%;
+          max-width: 320px;
+        }
       </style>
       <form>
-        ${filters.map(
-          ({ label, icon }, i) => html`
-            <input
-              type="radio"
-              name="filters"
-              id="${i}"
-              value="${i}"
-              ${i === 0 ? "checked" : ""}
-            />
-            <label for="${i}"
-              ><!--<svg class="icon">
+        <div>
+          ${filters.map(
+            ({ label, icon }, i) => html`
+              <input
+                type="radio"
+                name="filters"
+                id="${i}"
+                value="${i}"
+                ${i === 0 ? "checked" : ""}
+              />
+              <label for="${i}"
+                ><!--<svg class="icon">
                 <use xlink:href="#${icon}"></use></svg
               >-->${label}</label
-            >
-          `
-        )}
+              >
+            `
+          )}
+        </div>
+        <input
+          id="search"
+          type="text"
+          placeholder="Search…"
+          list="tag-suggestions"
+        />
       </form>
 
-      ${PostList(site.posts)}
+      <datalist id="tag-suggestions">
+        ${site.tags.map((tag) => html`<option value="#${tag}"></option>`)}
+      </datalist>
+
+      ${PostList(site.posts, true)}
 
       <script>
+        // @TODO support query params that search inline
+
+        const $search = document.querySelector("#search");
         const $lis = Array.from(document.querySelectorAll(".posts-list li"));
         document.querySelector("form").addEventListener("change", (e) => {
           switch (e.target.value) {
@@ -114,6 +149,7 @@ export default function Index(site) {
                 $li.removeAttribute("hidden");
               });
               sortList("data-date");
+              $search.removeAttribute("hidden");
               break;
             case "1":
               console.log("Show favorites", $lis);
@@ -125,6 +161,7 @@ export default function Index(site) {
                 }
               });
               sortList("data-favorites");
+              $search.setAttribute("hidden", true);
               break;
             case "2":
               console.log("Show popular");
@@ -136,10 +173,42 @@ export default function Index(site) {
                 }
               });
               sortList("data-pageviews");
+              $search.setAttribute("hidden", true);
               break;
             default:
           }
         });
+
+        document
+          .querySelector("form input[type=text]")
+          .addEventListener("input", (e) => {
+            console.log("Filter");
+            $lis.forEach(($li) => {
+              // @TODO <datalist> of tags to match
+              // @TODO do better string matching
+              const title = $li.querySelector("a").textContent.toLowerCase();
+              const date = $li.getAttribute("data-date");
+
+              // @TODO fix logic handling
+              if (e.target.value.startsWith("#")) {
+                const tags = $li.getAttribute("data-tags");
+                const q = e.target.value.split("#")[1];
+                console.log(q, tags);
+                if (tags.indexOf(q) !== -1) {
+                  $li.removeAttribute("hidden");
+                } else {
+                  $li.setAttribute("hidden", true);
+                }
+              } else if (
+                title.indexOf(e.target.value.toLowerCase()) !== -1 ||
+                e.target.value.startsWith(date.slice(0, 4))
+              ) {
+                $li.removeAttribute("hidden");
+              } else {
+                $li.setAttribute("hidden", true);
+              }
+            });
+          });
 
         function sortList(attr) {
           var ul = document.querySelector(".posts-list");
@@ -160,19 +229,25 @@ function PostList(posts, showPageviews = false) {
   return html`
     <ul class="posts-list">
       ${posts.map(
-        ({ favorites_index, permalink, title, pageviews, date }) => html`
+        ({ favorites_index, permalink, title, pageviews, date, tags }) => html`
           <li
             data-date="${date.toISOString().slice(0, 10).replace(/-/g, "")}"
             ${favorites_index >= 0 && `data-favorites='${favorites_index}'`}
             ${pageviews && `data-pageviews='${pageviews}'`}
+            ${tags && `data-tags="${tags.join(" ")}"`}
           >
             <time datetime="${date.toISOString()}">${toDateUI(date)}</time>
-            ${showPageviews &&
-            html`<small
-              >${pageviews > 1000
-                ? Math.round((pageviews / 1000) * 10) / 10 + "k"
-                : pageviews}</small
-            >`}
+            ${showPageviews && pageviews
+              ? html`<small
+                  >↗
+                  ${pageviews > 1000
+                    ? Math.round((pageviews / 1000) * 10) / 10 + "k"
+                    : pageviews}</small
+                >`
+              : ""}
+            ${favorites_index &&
+            html`<small title="Personal favorite">★</small>`}
+            <a href="${permalink}">${title}</a>
           </li>
         `
       )}
