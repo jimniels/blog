d699294b0bdf04cfc978b810d98aa4a4a3aba35a
Jim Nielsen <jimniels@gmail.com>
2022-03-04 08:57:40 -0700

    feat: support tags at beginning of documents

--- metalsmith.js
+++ metalsmith.js
@@ -158,13 +158,25 @@ let App = Metalsmith(__dirname)
      */
     console.time("|-- build:markdown");
     multimatch(Object.keys(files), "**/*.md").forEach((file) => {
-      let fileContentsByLine = files[file].contents.toString().split("\n");
+      let fileContentsByLine = files[file].contents
+        .toString()
+        .split("\n")
+        // shouldn’t ever be looking deeper than 20 lines in any given files
+        .slice(0, 20);
 
       // The post `title` is added as the first <h1> in the document
       // We need to pull that data out and add it as a meta item for use
       for (let i = 0; i < fileContentsByLine.length; i++) {
         let line = fileContentsByLine[i];
-        if (line.startsWith("# ")) {
+        if (/#[a-z]/.test(line)) {
+          // Tags come formatted like this:
+          // #hashtag #myThing #design
+          // files[file].tags = line.split(" ").map((tag) => tag.slice(1));
+          // Turn it into how the YAML front-matter is stored
+          files[file].tags = line.replace(/#/g, "");
+          // Remove
+          fileContentsByLine.splice(i, 1);
+        } else if (line.startsWith("# ")) {
           // get the title
           let title = line.replace("# ", "");
           files[file].title = title;
