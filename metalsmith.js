7d8d64ff47be33d7f233d48eaf1fc9b032ce3b95
Jim Nielsen <jimniels@gmail.com>
2022-03-24 00:17:13 -0600

    fix: image styles on post

--- metalsmith.js
+++ metalsmith.js
@@ -86,7 +86,7 @@ const renderer = {
   // Images
   html(html) {
     if (html.startsWith("<img")) {
-      return `<p>${html}</p>`;
+      return `<p class="image-container">${html}</p>`;
     }
     return html;
   },
