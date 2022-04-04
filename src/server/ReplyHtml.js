a00a088258a35e20b6af616a4e0b37093cd05b9c
Jim Nielsen <jimniels@gmail.com>
2022-04-04 09:26:55 -0600

    fix: add <br> tags

--- src/server/ReplyHtml.js
+++ src/server/ReplyHtml.js
@@ -12,24 +12,22 @@ export default function ReplyHtml({ postTags, postPath, siteOrigin }) {
   return html`
     <br />
     <br />
-    <p>&lt;/&gt;</p>
-    <p>
-      Reply via:
-      <a
-        href="mailto:spokes-probes.0w@icloud.com?subject=Re: blog.jim-nielsen.com${postPath}"
-        >Email</a
-      >,
-      <a href="https://twitter.com/jimniels">Twitter</a>
-    </p>
+    &lt;/&gt;
+    <br />
+    Reply via:
+    <a
+      href="mailto:spokes-probes.0w@icloud.com?subject=Re: blog.jim-nielsen.com${postPath}"
+      >Email</a
+    >,
+    <a href="https://twitter.com/jimniels">Twitter</a>
     ${Array.isArray(postTags) &&
     postTags.length > 0 &&
     html`
-      <p>
-        Tagged:
-        ${postTags
-          .map((tag) => html`<a href="${siteOrigin}/tags/#${tag}">#${tag}</a>`)
-          .join(", ")}
-      </p>
+      <br />
+      Tagged:
+      ${postTags
+        .map((tag) => html`<a href="${siteOrigin}/tags/#${tag}">#${tag}</a>`)
+        .join(", ")}
     `}
   `;
 }
