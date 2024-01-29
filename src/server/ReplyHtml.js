f7892042cfa29e8fa63d1fabbd035e49d50b1c34
Jim Nielsen <jimniels@gmail.com>
2024-01-28 22:34:06 -0700

    Update ReplyHtml.js

--- src/server/ReplyHtml.js
+++ src/server/ReplyHtml.js
@@ -24,41 +24,39 @@ export default function ReplyHtml({ post, site }) {
 
   return html`
     <hr />
-    <ul>
-      <li>
-        Reply via:
-        ${
-          /* Trippy: we gotta encode the "+" or we get bit by outlook
+
+    <p>
+      Comment? Reply via:
+      ${
+        /* Trippy: we gotta encode the "+" or we get bit by outlook
              https://webmasters.stackexchange.com/questions/15920/should-plus-be-encoded-in-mailto-hyperlinks */
-          ""
-        }
-        <a
-          href="mailto:jimniels%2Bblog@gmail.com?subject=Re:%20blog.jim-nielsen.com${postPath}"
-          >Email</a
-        >, <a href="https://twitter.com/jimniels">Twitter</a>,
-        <a href="https://mastodon.social/@jimniels">Mastodon</a>
-      </li>
-      ${Array.isArray(postTags) &&
-      postTags.length > 0 &&
-      html`
-        <li>
-          Tagged in:
-          ${postTags
-            .map(
-              (tag) => html`<a href="${siteOrigin}/tags/#${tag}">#${tag}</a>`
-            )
+        ""
+      }
+      <a
+        href="mailto:jimniels%2Bblog@gmail.com?subject=Re:%20blog.jim-nielsen.com${postPath}"
+        >Email</a
+      >, <a href="https://mastodon.social/@jimniels">Mastodon</a>, or
+      <a href="https://twitter.com/jimniels">Twitter</a>.
+    </p>
+    ${Array.isArray(postTags) &&
+    postTags.length > 0 &&
+    html`
+      <p>
+        Tagged in:
+        ${postTags
+          .map((tag) => html`<a href="${siteOrigin}/tags/#${tag}">#${tag}</a>`)
+          .join(", ")}
+      </p>
+    `}
+    ${relatedPosts.length
+      ? html`<p>
+          Other posts that link here:
+          ${relatedPosts
+            .map(({ title, path }) => html`<a href="${path}">${title}</a>`)
             .join(", ")}
-        </li>
-      `}
-      ${relatedPosts.length
-        ? html`<li>
-            Related posts linking here:
-            ${relatedPosts
-              .map(({ title, path }) => html`<a href="${path}">${title}</a>`)
-              .join(", ")}
-          </li>`
-        : ""}
-    </ul>
+        </p>`
+      : ""}
+
     <br />
   `;
 }
